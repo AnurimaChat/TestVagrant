f22e9999b069258c407e3370195c45d8a3064e9b
Anurima <anurima.chatterjee@bold.com>
2023-01-26 17:33:40 +0530

    added test cases for RCBTeam

--- src/RCBTeamTest.java
+++ src/RCBTeamTest.java
@@ -1,22 +1,29 @@
 import POJO.RCBTeam;
 import org.testng.annotations.Test;
+import static org.junit.Assert.*;
 
 public class RCBTeamTest {
 
     RCBTeam rcbTeam = TeamMapper.getMapperObject();
     @Test
-    public void validateForeignPlayersInTeam(){
-
-        System.out.println(rcbTeam.getName());
-
-
-
+    public void validateFourForeignPlayersInTeam(){
+        int countOfForeignPlayers=0;
+        for(int i=0;i<rcbTeam.getPlayer().size();i++) {
+            if(!rcbTeam.getPlayer().get(i).getCountry().equalsIgnoreCase("India")) {
+                countOfForeignPlayers++;
+            }
+        }
+        assertEquals(4,countOfForeignPlayers);
     }
     @Test
-    public void validateWicketKeeperInTeam(){
-
-
-
-
+    public void validateAtLeastOneWicketKeeperInTeam(){
+        int countOfWicketKeeper=0;
+        for(int i=0;i<rcbTeam.getPlayer().size();i++) {
+            if(rcbTeam.getPlayer().get(i).getRole().equalsIgnoreCase("Wicket-keeper")) {
+                countOfWicketKeeper++;
+            }
+        }
+        assertTrue(countOfWicketKeeper>=1);
     }
+
 }
