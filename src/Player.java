0347229b98925d5c712701fe399c407ec2d2a347
Anurima <anurima.chatterjee@bold.com>
2023-01-26 11:59:56 +0530

    created POJO classes

---
+++ src/Player.java
@@ -0,0 +1,40 @@
+public class Player {
+    private String name;
+    private String country;
+    private String role;
+    private int price_in_crores;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getPrice_in_crores() {
+        return price_in_crores;
+    }
+
+    public void setPrice_in_crores(int price_in_crores) {
+        this.price_in_crores = price_in_crores;
+    }
+
+
+
+}
---
+++ src/RCBTeams.java
@@ -0,0 +1,32 @@
+public class RCBTeams {
+    private String name;
+    private String location;
+    private Player player;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+
+
+}
